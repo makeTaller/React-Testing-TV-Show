511e22dd4b59870db2215f469c257958366a839a
Kirk Tolliver <kirktolliver28@gmail.com>
2020-07-12 17:33:02 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
-import { fetchShow } from "./api/fetchShow";
 
 import { formatSeasons } from "./utils/formatSeasons";
-
+import fetchShow from './api/fetchShow'
 import Episodes from "./components/Episodes";
 import "./styles.css";
 
@@ -16,11 +14,13 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    console.log(fetchShow())
-      /* .then(res => {
-       *   setShow(res.data);
-       *   setSeasons(formatSeasons(res.data._embedded.episodes));
-       * }); */
+    fetchShow()
+        .then(res => {
+          setShow(res.data);
+          setSeasons(formatSeasons(res.data._embedded.episodes));
+        });
+    
+    
   }, []);
 
   const handleSelect = e => {
--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,12 +1,14 @@
-import React, {useEffect} from "react";
-import axios from "axios";
+import axios from 'axios';
 
-export const fetchShow = () => {
-        axios
-            .get(
-                "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-            )
-            .then(res => {
-               return res.data;
-            });
-    };
+
+ const fetchShow = () => {
+   return axios
+      .get(
+        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+      )
+      .then(res => {
+        return res;
+      });
+  };
+
+  export default fetchShow;
